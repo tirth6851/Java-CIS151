78e4e534f8cdc69253bca05eb4d49844c1419d58
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-10-16 18:06:28 -0400

    big upd

---
+++ brocode/code_version_1/fistprogram_test.java
@@ -0,0 +1,23 @@
+package brocode;
+
+public class fistprogram_test {
+  public static void main(String[] args){
+    int age = 19;
+    String gender = "Male";
+    Double gpa = 3.68;
+    char fullstop = '.';
+    char comma = ',';
+    char apha_A = 'A';
+    
+    System.out.println(age + " " + gender + " and" + comma + " thier gpa is = " + gpa + " OR " + apha_A + fullstop);
+    
+    boolean isStudent = true;
+    boolean notStudent = false;
+
+    if(isStudent) {
+      System.out.println("Yes is a student");
+    }
+    
+
+  }
+}

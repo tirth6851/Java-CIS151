dab63ff621eb80860fe5f47adcf1ea05667a5919
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-10-27 20:38:11 +0000

    upgrades

---
+++ brocode/User-Defined Methods/prin.java
@@ -0,0 +1,10 @@
+public  class prin {
+  public static void main(String[] args) {
+    //System.out.println("hi");
+  }
+  static void hello() {
+    string h;
+    h = System.out.println("hello");
+    return h;
+  }
+}

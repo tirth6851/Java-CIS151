4a76d4b6110f23ad77095e313687a5ed35b6168b
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-10-27 21:25:53 +0000

    upts

--- brocode/User-Defined-Methods/prin.java
+++ brocode/User-Defined-Methods/prin.java
@@ -1,10 +1,10 @@
 public  class prin {
   public static void main(String[] args) {
     //System.out.println("hi");
+    System.out.println(hello());
   }
-  static void hello() {
-    string h;
-    h = System.out.println("hello");
+  static String hello() {
+    String h = "hello";
     return h;
   }
 }

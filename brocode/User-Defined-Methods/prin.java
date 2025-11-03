bd1fcceec498a61eef3addd6159d251c7168478a
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-11-03 20:05:48 +0000

    loop

--- brocode/User-Defined-Methods/prin.java
+++ brocode/User-Defined-Methods/prin.java
@@ -2,34 +2,24 @@ public  class prin {
   public static void main(String[] args) {
     //System.out.println("hi");
     System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
-    System.out.println(hello());
+    
+
+    //loop
+    for(int i; i)
+  
+  
   }
   static String hello() {
     String h = "hello";
     return h;
   }
+
+
+  
+  // testing git commit0
+  // testing git commit1
+  
+
+  
+  
 }
-// testing git commit0
-// testing git commit1

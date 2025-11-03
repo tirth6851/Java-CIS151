93efee8d466faeb120d219b63d59131f49d588d4
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-11-03 22:38:20 +0000

    loops

---
+++ brocode/loop/loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class loop {
+  public static void  main(String[] args) {
+
+    //defining scanner
+    Scanner sc = new Scanner(System.in);
+
+
+    int target = 50;
+    while (target > 0) {
+      System.out.println(target);
+      target -= 5;
+    }
+
+  }
+}

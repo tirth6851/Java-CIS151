9fa587faa29e5a5592a60084bc76a5cd29311c12
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-11-14 11:47:39 +0000

    nest loop upt

--- brocode/loop/forLoopNest_sqr.java
+++ brocode/loop/forLoopNest_sqr.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
 
-public class forLoop_N {
+public class forLoopNest_sqr {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     //scan.nextLine();
+    //System.out.println("test");
 
+    System.out.print("Number of columns: ");
+    int col = scan.nextInt();
+    System.out.print("Number of rows: ");
+    int row = scan.nextInt();
+    System.out.println();
 
-
-    for (int i = 1; i < 3; i++) {
-      System.out.print("*");
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        System.out.print(" *");
+      }
+      System.out.println();
     }
     scan.close();
   }  

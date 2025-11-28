4a13ae8169c8aa7781aaffbccdb357daa057cdf2
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-11-28 22:31:00 +0000

    lab 3 done

--- labs/GPT/Lab3EvenOdd/Lab3EvenOdd.java
+++ labs/GPT/Lab3EvenOdd/Lab3EvenOdd.java
@@ -6,13 +6,27 @@ public class Lab3EvenOdd {
     System.out.print("what is N: ");
     int n =  scan.nextInt();
     System.out.print("All: ");
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i <= n; i++) {
       System.out.print(i + " ");
+    }
+    System.out.println();
+
+    System.out.print("Even: ");
+    for(int i = 1; i <= n; i++) {
       if(i % 2 == 0) {
-        System.out.print("Even: ");
+        System.out.print(i + " ");
+      }
+    } 
+    System.out.println();
+    System.out.print("Odd: ");
+    for(int i = 1; i <= n; i++) {
+      if(i % 2 != 0) {
         System.out.print(i + " ");
       }
     }
+    System.out.println();
+
+
 
     scan.close();
   }

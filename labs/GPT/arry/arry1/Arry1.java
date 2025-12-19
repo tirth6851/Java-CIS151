d29957b7c892a3b4d8672d84c77f6d093f97a241
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-12-19 17:29:58 +0000

    upt

---
+++ labs/GPT/arry/arry1/Arry1.java
@@ -0,0 +1,20 @@
+public class Arry1 {
+
+    public static void main(String[] args) {
+
+        // TODO 1: Declare an integer array named nums with 5 values
+        
+
+        // TODO 2: Print the first element of the array
+        
+
+        // TODO 3: Print the last element of the array
+        // Hint: use nums.length
+        
+
+        // TODO 4: Use a for loop to print all elements in the array
+        // Hint: loop from 0 to nums.length - 1
+        
+
+    }
+}
